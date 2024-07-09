d3feee7167c0bd447460551289fb5294ffb9e351
Doit-Ay <125307797+Doit-Ay@users.noreply.github.com>
2024-07-09 18:45:01 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.getElementById('navbar');
     
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
+        if (window.scrollY > 100) {
             navbar.classList.add('scrolled');
         } else {
             navbar.classList.remove('scrolled');
